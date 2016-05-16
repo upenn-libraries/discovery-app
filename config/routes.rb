46de79e5e4868e9033113de560e61b4fc1ec9b6d
Katherine Lynch <katherly@upenn.edu>
2016-05-16 14:45:39 -0400

    Added blacklight_range_limit and ran generators

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
           mount Blacklight::Engine => '/'
 
   Blacklight::Marc.add_routes(self)
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
 
 resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
   concerns :searchable
+    concerns :range_searchable
+
 end
 
   devise_for :users
