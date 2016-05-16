e4248ecc08ab3be0f15801eeb8abe217f66668f3
Katherine Lynch <katherly@upenn.edu>
2016-05-16 14:48:10 -0400

    Added pub_date range limit facet, changed site_name to a link in header, removed autofocus on search box for accessibility

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -72,20 +72,19 @@ class CatalogController < ApplicationController
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
     config.add_facet_field 'format', label: 'Format'
-    config.add_facet_field 'pub_date', label: 'Publication Year', single: true
     config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 20, index_range: 'A'..'Z'
     config.add_facet_field 'language_facet', label: 'Language', limit: true
     config.add_facet_field 'lc_1letter_facet', label: 'Call Number'
     config.add_facet_field 'subject_geo_facet', label: 'Region'
     config.add_facet_field 'subject_era_facet', label: 'Era'
-
     config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
-
     config.add_facet_field 'example_query_facet_field', label: 'Publish Date', :query => {
        :years_5 => { label: 'within 5 Years', fq: "pub_date:[#{Time.zone.now.year - 5 } TO *]" },
        :years_10 => { label: 'within 10 Years', fq: "pub_date:[#{Time.zone.now.year - 10 } TO *]" },
        :years_25 => { label: 'within 25 Years', fq: "pub_date:[#{Time.zone.now.year - 25 } TO *]" }
     }
+    config.add_facet_field 'pub_date_sort', label: 'Publication Year', range: true, collapse: false
+
 
 
     # Have BL send all facet field names to Solr, which has been the default
