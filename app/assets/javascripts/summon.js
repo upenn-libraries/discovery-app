4c46f975322c22cf7f0024bb3d20d5430daa20db
Jeff Chiu <jeffchiu@upenn.edu>
2016-11-21 15:55:07 -0500

    convert landing page; hook up to bento box functionality and summon search

---
+++ app/assets/javascripts/summon.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(".summon-form").submit(function(event) {
+        event.preventDefault();
+        var searchURL = "http://upenn.summon.serialssolutions.com/search#!/search?q=" + encodeURIComponent($(".summon-form input[name='q']").val());
+        // Must concatenate all fvf values into one parameter.
+        var fvf = "";
+        $(".summon-form .limit-options input:checked").each(function() {
+            if (this.name === "fvf") {
+                if (fvf !== "") {
+                    fvf += "|";
+                }
+                fvf += this.value;
+            } else {
+                searchURL += "&" + this.name + "=" + this.value;
+            }
+        });
+        if (fvf !== "") {
+            searchURL += "&fvf=" + fvf;
+        }
+        window.location.href = searchURL;
+    });
+});
