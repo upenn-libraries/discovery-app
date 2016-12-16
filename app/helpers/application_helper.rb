f2083ffd38b2f5326a724fdb64d1256af817a743
Jeff Chiu <jeffchiu@upenn.edu>
2016-12-16 17:03:05 -0500

    fix wrong way of doing blacklight helper overrides; add note about advanced search helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,6 @@ module ApplicationHelper
     return "http://upenn.summon.serialssolutions.com/search#!/search?q=#{url_encode(query)}"
   end
 
-  # override Blacklight to replace the default bar (containing simple search form)
-  # with our fancy tabbed search bar
-  def render_search_bar
-    render partial: 'catalog/franklin_search_bar'
-  end
-
   # returns the css classes needed for elements that should be considered 'active'
   # with respect to tabs functionality
   def active_tab_classes(tab_id)
@@ -45,23 +39,6 @@ module ApplicationHelper
     content_tag('a', tab_label, attrs)
   end
 
-  # override Blacklight so that 'index_document_append' and
-  # 'show_document_append' partials are appended to the partials
-  # that normally render for a document. This exists
-  # to avoid having to copy-and-paste a stock BL template
-  # when we want to append to it; that prevents us from
-  # getting template updates when upgrading BL.
-  def render_document_partial(doc, base_name, locals = {})
-    result = super(doc, base_name, locals)
-    if [:index, :show].member?(base_name)
-      template = lookup_context.find_all("#{base_name}_document_append", lookup_context.prefixes + [""], true, locals.keys + [:document], {}).first
-      if template
-        result += template.render(self, locals.merge(document: doc))
-      end
-    end
-    result
-  end
-
   # suppress showing availability info, which we want to do in Test
   # currently but not Dev.
   # TODO: should be removed eventually
@@ -69,21 +46,12 @@ module ApplicationHelper
     !(ENV['SUPPRESS_AVAILABILITY'] == 'true')
   end
 
-  # override Blacklight so Start Over always goes to catalog start page
-  def start_over_path(query_params = params)
-    # we do NOT call #search_action_path because it might take us to an
-    # "blank" browse page, which is never what we want
-    root_path
-  end
-
+  # TODO: this isn't guaranteed to work; we need AdvancedHelper
+  # to be overrideable like BlacklightHelper
   def facet_field_names_for_advanced_search
     # exclude pub date range facet, b/c it has a form, which nests insid
     # the advanced search form and causes havoc
     blacklight_config.facet_fields.values.select { |f| !f.xfacet && f.field != 'pub_date_isort' }.map(&:field)
-  end
-
-  def render_link_to_range_limit(solr_field, min, max)
-    link_to('View distribution', params.merge(use_route: "blacklight_advanced_search_routes", :action => 'range_limit', :range_field => solr_field, :range_start => min, :range_end => max), :class => "load_distribution")
   end
 
 end
