2f58cbf4f63748d5184b796898174bba7875b9ae
Jeff Chiu <jeffchiu@upenn.edu>
2016-12-19 10:12:36 -0500

    add AdvancedHelper override and rm corresponding method from ApplicationHelper; use my github for most recent blacklight_advanced_search changes; conflict bet adv search and blacklight_solrplugins seems to have been fixed by e5e4d27 in adv search

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -2,17 +2,7 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include BlacklightAdvancedSearch::AdvancedSearchBuilder
-  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr]
+  self.default_processor_chain += [:add_advanced_parse_q_to_solr, :add_advanced_search_to_solr ]
   include BlacklightRangeLimit::RangeLimitBuilder
-
-  # overrides BlacklightAdvancedSearch::AdvancedSearchBuilder#add_advanced_parse_q_to_solr
-  # we need the check in this override to prevent browse pages from getting the q param
-  # TODO: we can remove this once this PR gets accepted:
-  # https://github.com/projectblacklight/blacklight_advanced_search/pull/69
-  def add_advanced_parse_q_to_solr(solr_parameters)
-    if is_advanced_search?
-      super(solr_parameters)
-    end
-  end
 
 end
