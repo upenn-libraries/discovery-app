2c45ee67e8cb6e4fd01d5cb4a18defad0d812ec2
Jeff Chiu <jeffchiu@upenn.edu>
2017-01-09 16:24:45 -0500

    revert unnecessary changes to availability.js

--- app/assets/javascripts/availability.js
+++ app/assets/javascripts/availability.js
@@ -1,19 +1,4 @@
 $(document).ready(function() {
-
-    function BlacklightAlmaCustom() {
-    };
-
-    BlacklightAlmaCustom.prototype = new BlacklightAlma();
-
-    /*
-    BlacklightAlmaCustom.prototype.loadAvailability = function(){
-        BlacklightAlma.prototype.loadAvailability.call(this);
-        console.log("I'm here!")
-        $(".show-on-availability-ajax-load").removeClass("hide").show();
-    };
-    */
-
-    var ba = new BlacklightAlmaCustom();
+    var ba = new BlacklightAlma();
     ba.loadAvailability();
-
 });
