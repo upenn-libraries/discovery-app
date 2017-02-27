294a41834496a25872b2a8189edc4c299857bdc0
Jeff Chiu <jeffchiu@upenn.edu>
2017-02-27 10:47:13 -0500

    check for physical holdings 1st when determining getit/viewit, b/c that's more important: electronic holdings may be represented in the UI in other ways

--- app/helpers/blacklight_alma_helper.rb
+++ app/helpers/blacklight_alma_helper.rb
@@ -3,10 +3,10 @@ module BlacklightAlmaHelper
   include BlacklightAlma::HelperBehavior
 
   def alma_service_type_for_fulfillment_url(document)
-    if document.has?('electronic_holdings_json').present?
-      'viewit'
-    else
+    if document.has?('physical_holdings_json').present?
       'getit'
+    else
+      'viewit'
     end
   end
 
