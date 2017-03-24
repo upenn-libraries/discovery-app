9a532fef3ee3045d0608d78a7a138d826b489335
Jeff Chiu <jeffchiu@upenn.edu>
2017-03-24 14:59:53 -0400

    allow indexing and preprocessing scripts to take multiple arguments

--- index_solr.sh
+++ index_solr.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#
-# Usage: ./index_solr.rb "/path/to/files/*.xml"
-#
-# Note the quoted glob.
 
-time ./process_files.rb -l log/indexing -p 4 -i "$1" index_into_solr
+time ./process_files.rb -l log/indexing -p 4 -s index_into_solr "$@" 
