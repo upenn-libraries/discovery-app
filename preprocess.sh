9ee549bb5de0bfd3b2594fb9d1209314cbd78a0e
Jeff Chiu <jeffchiu@upenn.edu>
2017-03-28 09:58:32 -0400

    handle file deletion using runtime flags instead of coding them in steps; make chdir part of DSL

--- preprocess.sh
+++ preprocess.sh
@@ -7,10 +7,10 @@ dir=`dirname "$1"`
 GLOB_DIR=${GLOB_DIR:-$dir}
 
 # first pass: fix XML namespace and create boundwith_.*xml files
-time ./process_files.rb -x $XSL_DIR -p 4 -s fix_namespace,create_bound_withs "$@"
+time ./process_files.rb -x $XSL_DIR -p 4 -i -s fix_namespace,create_bound_withs "$@"
 
 # index the derived boundwith_*.xml files into a sqlite database
 time bundle exec rake pennlib:marc:create_boundwiths_index BOUND_WITHS_DB_FILENAME=bound_withs.sqlite3 BOUND_WITHS_GLOB="$GLOB_DIR/boundwiths_*.xml"
 
 # fix up the MARC and merge in the boundwith holdings
-time ./process_files.rb -x $XSL_DIR -p 4 -s fix_marc,merge_bound_withs,format,rename_to_final_filename "$@"
+time ./process_files.rb -x $XSL_DIR -p 4 -i -s fix_marc,merge_bound_withs,format,rename_to_final_filename "$@"
