1546db57045035a0da8c46841cd9ed4ba5a28287
Jeff Chiu <jeffchiu@upenn.edu>
2017-04-05 11:32:26 -0400

    add more log output

--- preprocess_oai.sh
+++ preprocess_oai.sh
@@ -19,11 +19,12 @@ export BOUND_WITHS_XML_DIR="$INPUT_FILES_DIR"
 
 export XSL_DIR=${XSL_DIR:-$SCRIPT_DIR/xsl}
 
-# first pass: convert OAI to MARC and create boundwith_.*xml files
-time ./process_files.rb -p 4 -s convert_oai_to_marc,create_bound_withs "$@"
+echo "Converting OAI to MARC and creating boundwith_.*xml files"
+./process_files.rb -p 4 -s convert_oai_to_marc,create_bound_withs "$@"
 
-# index the derived boundwith_*.xml files into a sqlite database
-time bundle exec rake pennlib:marc:create_boundwiths_index
+echo "Indexing derived boundwith_*.xml files into a sqlite database"
+bundle exec rake pennlib:marc:create_boundwiths_index
 
-# fix up the MARC and merge in the boundwith holdings; note that we use -d and -i to delete original and intermediate files in this pipeline
-time ./process_files.rb -d -i -p 4 -s fix_marc,merge_bound_withs,format,rename_to_final_filename "$INPUT_FILES_DIR/*_marc.xml"
+echo "Fixing MARC and merging in boundwith holdings"
+# note that we use -d and -i to delete original and intermediate files in this pipeline
+./process_files.rb -d -i -p 4 -s fix_marc,merge_bound_withs,format,rename_to_final_filename "$INPUT_FILES_DIR/*_marc.xml"
