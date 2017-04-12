39a1f5483c1e90b4372a6de274b35bbbd120f03d
Jeff Chiu <jeffchiu@upenn.edu>
2017-04-12 10:30:39 -0400

    display availability on title browse page

--- app/helpers/blacklight_solrplugins_helper.rb
+++ app/helpers/blacklight_solrplugins_helper.rb
@@ -6,4 +6,13 @@ module BlacklightSolrpluginsHelper
     link_to(doc_presenter.field_value('title'), solr_document_path(doc_presenter.field_value('id')))
   end
 
+  def render_rbrowse_display_field(fieldname, doc_presenter)
+    # handle special case of availability, which gets loaded via ajax
+    if fieldname == 'availability'
+      render partial: 'status_location_field', locals: { document: doc_presenter.document }
+    else
+      super(fieldname, doc_presenter)
+    end
+  end
+
 end
