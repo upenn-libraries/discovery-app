5788f070cf8adef6b20b083b7529a7b12b16e21c
Chris Clement <clemenc@upenn.edu>
2017-07-13 11:58:31 -0400

    Added 3rd party cookie check

---
+++ app/assets/javascripts/3rdpartycookiecheck.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var receiveMessage = function (evt) {
+    if (evt.data === 'MM:3PCunsupported') {
+      flashContainer = document.getElementsByClassName('flash_messages')[0];
+      if(flashContainer) {
+        flashContainer.innerHTML += " \
+          <div class='alert alert-warning'> \
+            NOTE: 3rd party cookies are disabled. \
+            <a class='close' data-dismiss='alert' href='#'>×</a> \
+          </div>";
+      }
+    }
+  };
+  window.addEventListener("message", receiveMessage, false);
+});
