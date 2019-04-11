bf87d510f2137e90afa7fd33e7d1ce0e2be3acbe
Chris Clement <clemenc@upenn.edu>
2019-04-11 11:14:27 -0400

    Missed a JS 'let' from previous commit

--- app/assets/javascripts/request_option_highlight.js
+++ app/assets/javascripts/request_option_highlight.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('body').on('click','.request-option-link', function(event) {
-        let table = $(event.target).closest('div[id^=table-wrapper')
+        var table = $(event.target).closest('div[id^=table-wrapper')
         table.find('button.highlightable').each(function() {
           $(this).parent().addClass('highlighted');
           $(this).animate({backgroundColor: '#f3f6ba'}, 1000);
