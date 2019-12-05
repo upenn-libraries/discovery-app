3e5129ad2f6e0aaf0911c341a8c5d591ada0d8fc
Michael Gibney <michael@michaelgibney.net>
2019-12-05 10:42:36 -0500

    update collection news blog feed, per vallhonr

--- app/controllers/collection_news_controller.rb
+++ app/controllers/collection_news_controller.rb
@@ -7,7 +7,7 @@ class CollectionNewsController < ApplicationController
   include RssProxy
 
   def index
-    rss_proxy('https://pennlibnews.wordpress.com/category/collection-news/feed/')
+    rss_proxy('http://www.library.upenn.edu/blogs/libraries-news/category/Collections/rss.xml')
   end
 
 end
