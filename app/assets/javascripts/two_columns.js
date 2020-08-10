a498b1d1e55305bb57bc40b6cd6edbb9a46a52be
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2020-08-10 12:58:19 -0400

    arrange bento into 3 columns that do not hide with no results

--- app/assets/javascripts/two_columns.js
+++
@@ -1,6 +0,0 @@
-$(document).ajaxComplete(function() {
-    if($("#bento-results-databases .bento-item").length == 0 && $('.gs-no-results-result').length){
-        $('#bento-results-catalog').addClass('fifty-percent')
-        $('#bento-results-summon').addClass('fifty-percent')
-    }
-});
