291a251fc69c3e12396c8d8f05a7b4a8fefed663
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2020-08-20 10:28:01 -0400

    clean up unwanted changes

--- lib/blacklight/solr/response/subject_specialists.rb
+++ lib/blacklight/solr/response/subject_specialists.rb
@@ -1,5 +1,3 @@
-# require "active_support/core_ext/hash/indifferent_access"
-
 module Blacklight::Solr::Response::SubjectSpecialists
   class Constants
     SPECIALISTS = JSON.parse(File.read(Rails.root.join('config', 'translation_maps', 'expert_help_directory.json'))).with_indifferent_access
