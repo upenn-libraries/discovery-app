6d6a7822a5b125cad9f413bcf63f329c97852ea8
Mike Kanning <mkanning@upenn.edu>
2020-08-25 17:13:59 -0400

    progress

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -133,6 +133,8 @@ class CatalogController < ApplicationController
         bound_with_ids_a
         marcrecord_text
         recently_added_isort
+        hld_count_isort
+        prt_count_isort
       }.join(','),
         'facet.threads': 2,
         'facet.mincount': 0,
@@ -798,6 +800,7 @@ class CatalogController < ApplicationController
 
   # extend 'index' so we can override views
   def bento
+    params[:per_page] = 5
     index
   end
 
