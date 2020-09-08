60bf3809e96c1644433e1439e3fad07044371ea6
Mike Kanning <mkanning@upenn.edu>
2020-09-08 11:14:07 -0400

    refactor subject specialist data retrieval methods

--- lib/blacklight/solr/response/subject_specialists.rb
+++ lib/blacklight/solr/response/subject_specialists.rb
@@ -7,34 +7,24 @@ module Blacklight::Solr::Response::SubjectSpecialists
       live_specialists_data = JSON.parse(Faraday.get(specialists_url).body)
       live_specialists_data.each do |specialty|
         # nasty way to make the subject hash key match Drupal anchor tag ids
-        subject_key = specialty["subject_specialty"].gsub(/[&#;]/,"").parameterize.underscore
-
-        specialty = specialty.map { |k, value| [k, CGI.unescapeHTML(value)] }.to_h
-        name = specialty["full_name"].parameterize.underscore
-
-        subject[subject_key] = [] unless subjects[subject_key]
-        subject[subject_key] << name
-          subjects[subject_key] << name
-        else
-          subjects[subject_key] = [name]
-        end
-
+        subject_key = specialty['subject_specialty'].gsub(/[&#;]/, '')
+                                                    .parameterize.underscore
+        specialty = specialty.transform_values { |v| CGI.unescapeHTML v }
+        name = specialty['full_name'].parameterize.underscore
+        subjects[subject_key] = [] unless subjects[subject_key]
+        subjects[subject_key] << name
         if specialists[name]
-          specialists[name][:subjects] << specialty["subject_specialty"]
+          specialists[name][:subjects] << specialty['subject_specialty']
         else
-          specialty[:subjects] = [specialty["subject_specialty"]]
-          specialty[:display_name] = specialty["full_name"]
-          specialty[:portrait] = "https://www.library.upenn.edu#{specialty["thumbnail"]}"
+          specialty[:subjects] = [specialty['subject_specialty']]
+          specialty[:display_name] = specialty['full_name']
+          specialty[:portrait] = "https://www.library.upenn.edu#{specialty['thumbnail']}"
           specialists[name] = specialty
         end
       end
-
       subjects.each do |subject, staff|
-        subjects[subject] = staff.map{ |name| specialists[name] }
+        subjects[subject] = staff.map { |name| specialists[name] }
       end
-
-      subjects
     end
-
   end
 end
