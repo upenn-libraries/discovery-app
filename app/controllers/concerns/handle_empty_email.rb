20d419f98881cd77bd7aea0f7f0b4f1f5859e004
Mike Kanning <mkanning@upenn.edu>
2020-09-21 14:42:59 -0400

    retry sending emails from catalog show pages if a Net::ReadTimeout occurs. Report to Honeybadger if things repeatedly fail.

--- app/controllers/concerns/handle_empty_email.rb
+++ app/controllers/concerns/handle_empty_email.rb
@@ -1,16 +1,34 @@
-
+# This module overrides Blacklight's mail-sending methods to only
+# actually send an email if there are documents selected.
+# MK 9/2020 - Why? Dunno. Maybe crawlers hit the urls and this was throwing
+# exceptions?
 module HandleEmptyEmail
 
   extend ActiveSupport::Concern
 
-  # override
+  # override of method in Blacklight::Catalog
   def email_action(documents)
-      super(documents) if documents.length > 0
+    return unless documents.any?
+
+    begin
+      retries ||= 0
+      super(documents)
+    rescue Net::ReadTimeout => e
+      sleep 3
+      if (retries += 1) < 2
+        retry
+      else
+        Honeybadger.notify e
+      end
+    end
   end
 
-  # override
+  # override of method in Blacklight::Catalog
+  # Note: Penn seems to have disabled the links to send a SMS
   def sms_action(documents)
-      super(documents) if documents.length > 0
+    return unless documents.any?
+
+    super(documents)
   end
 
 end
