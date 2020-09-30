4bc88a1f168105b9a69217d8401a8e883d18fe23
Michael Gibney <michael@michaelgibney.net>
2020-09-30 16:35:59 -0400

    change META_CHARS from class variable to constant

--- lib/penn_lib/summon_engine.rb
+++ lib/penn_lib/summon_engine.rb
@@ -1,6 +1,8 @@
 
 class PennLib::SummonEngine < BentoSearch::SummonEngine
 
+  META_CHARS = '-|!\(\){}\[\]^~*?\\:' # '&' does not seem to be affected
+
   def search_implementation(args)
     mitigate(args)
     results = super(args)
@@ -9,8 +11,6 @@ class PennLib::SummonEngine < BentoSearch::SummonEngine
     end
     return results
   end
-
-  @@META_CHARS = '-|!\(\){}\[\]^~*?\\:' # '&' does not seem to be affected
 
   # The stock summon_engine does escape '+', but as of 20200930 it appears that
   # no matter how deeply you try to escape meta-characters, a trailing '+' still
@@ -21,13 +21,13 @@ class PennLib::SummonEngine < BentoSearch::SummonEngine
   # issue is addressed, but should also be harmless to leave in place.
   def mitigate(args)
     query = args[:query]
-    query.gsub!(/(^|[[:space:]])\+-[#{@@META_CHARS}]*([^#{@@META_CHARS}])/) do |match|
+    query.gsub!(/(^|[[:space:]])\+-[#{META_CHARS}]*([^#{META_CHARS}])/) do |match|
       # replace extraneous META_CHARS following any '+-'-led clause
       "#{$1}+#{$2}"
     end
     plus_idx = query.rindex('+')
     return unless plus_idx # no '+' char; not applicable
-    if query[plus_idx + 1..-1].match(/[^[[:space:]]#{@@META_CHARS}]/)
+    if query[plus_idx + 1..-1].match(/[^[[:space:]]#{META_CHARS}]/)
       # there's some other character after the last '+' that will cause
       # the query to parse properly. Such ameliorating characters are neither whitespace
       # nor special syntax characters, and mean the original query should parse fine as-is
