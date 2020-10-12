972d2d16132286329a80a112a61590aeec07fd4f
Michael Gibney <michael@michaelgibney.net>
2020-10-12 16:34:12 -0400

    extract and clarify (comment) navigation in hierarchical facets

--- app/helpers/blacklight/hierarchy_helper.rb
+++ app/helpers/blacklight/hierarchy_helper.rb
@@ -4,8 +4,7 @@ module Blacklight::HierarchyHelper
   # than either render(:partial) or content_tag
   def render_facet_hierarchy_item(facet_item, facet_config, level)
     field_name = facet_item.field
-    hierarchy_components = facet_config[:sub_hierarchy]
-    subset = level < hierarchy_components.size ? facet_item.subs&.[](hierarchy_components[level]) : nil
+    subset = subfacet(level, facet_config, facet_item)
     li_class = subset.blank? ? 'h-leaf' : 'h-node'
     ul = ''
     li = if facet_in_params?(field_name, facet_item.value)
@@ -21,6 +20,15 @@ module Blacklight::HierarchyHelper
       ul = "<ul>#{subul}</ul>".html_safe
     end
     %(<li class="#{li_class}">#{li.html_safe}#{ul.html_safe}</li>).html_safe
+  end
+
+  # `facet_config[:sub_hierarchy]` specifies the keys that allow us to navigate down
+  # through subfacets. If `level < hierarchy_components.size` then we know there is
+  # another level of facets, and we retrieve it from the `parent_facet_item` subs using
+  # the key specified for our current `level`
+  def subfacet(level, facet_config, parent_facet_item)
+    hierarchy_components = facet_config[:sub_hierarchy]
+    return level < hierarchy_components.size ? parent_facet_item.subs&.[](hierarchy_components[level]) : nil
   end
 
   # @param [Blacklight::Configuration::FacetField] as defined in controller with config.add_facet_field (and with :partial => 'blacklight/hierarchy/facet_hierarchy')
