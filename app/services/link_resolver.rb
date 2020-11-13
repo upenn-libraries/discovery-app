20b46e6cc36a21667857a63adaa84b2bf95df019
Mike Kanning <mkanning@upenn.edu>
2020-11-13 14:45:02 -0500

    add notes. remove unneeded query params to LinkResovler lookup

--- app/services/link_resolver.rb
+++ app/services/link_resolver.rb
@@ -3,14 +3,13 @@ class LinkResolver
   # @return [String] resolution_url for mmsid
   def self.resolution_url_for(mms_id)
     url = 'https://upenn.alma.exlibrisgroup.com/view/uresolver/01UPENN_INST/openurl?'\
-            '&u.ignore_date_coverage=true'\
             "&rft.mms_id=#{mms_id}"\
-            '&rfr_id=info:sid/primo.exlibrisgroup.com'\
             '&svc_dat=CTO'
     response = HTTParty.get url
     return unless response.success?
 
     doc = Nokogiri::XML(response.body)
+    # TODO: what if >1 resolution_url? may obtain with >1 context_service in response
     doc.search('resolution_url').text
   end
 end
