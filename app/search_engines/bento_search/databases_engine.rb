8285597969937eb96e7f6d4d41f037e91ddfcd8e
Mike Kanning <mkanning@upenn.edu>
2021-08-30 17:19:32 -0400

    use a constant for the "Databases & Article Indexes" string used throughout the app. add a top-level route that redirects to the databases search page.

--- app/search_engines/bento_search/databases_engine.rb
+++ app/search_engines/bento_search/databases_engine.rb
@@ -36,7 +36,7 @@ class BentoSearch::DatabasesEngine
       per_page: 5,
       facet: false,
       search_field: 'keyword',
-      f: { 'format_f': ['Database & Article Index'] }
+      f: { 'format_f': [PennLib::SubjectConfig::DATABASES_FACET_VALUE] }
     )
     bl_results.first.docs
   end
