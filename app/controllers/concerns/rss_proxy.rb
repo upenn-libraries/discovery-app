e2ca0d469879792e7f12569013644c89348c5f87
Mike Kanning <mkanning@upenn.edu>
2021-10-04 06:24:28 -0400

    improve use of Typhoeus in rss_proxy concern

--- app/controllers/concerns/rss_proxy.rb
+++ app/controllers/concerns/rss_proxy.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 # caches in memory and on disk
 class RSSCache
@@ -17,14 +18,14 @@ class RSSCache
 
   # returns data for key, nil if nothing is cached
   def get(key)
-    if !@store[key].present?
+    unless @store[key].present?
       path = path_for_key(key)
       if File.exists?(path)
         begin
           data = File.open(path, 'r') { |f| Marshal.load(f) }
           @store[key] = data
         rescue Exception => e
-          Rails.logger.error("Something went wrong reading RSSCache file from disk: #{path} #{e}")
+          Honeybadger.notify("Something went wrong reading RSSCache file from disk: #{path} #{e}")
         end
       end
     end
@@ -44,44 +45,32 @@ module RssProxy
   extend ActiveSupport::Concern
 
   # make an http request; this handles redirects
+  # @param [String] url
+  # @param [Fixnum] limit
   def make_request(url, limit = 0)
-    if limit > 5
-      return nil
-    end
-    # uri = URI.parse(url)
-    # req = Net::HTTP::Get.new(uri)
-    #
-    # http = Net::HTTP.new(uri.host, uri.port)
-    # # relatively short timeout to prevent taking up rails processes
-    # http.read_timeout = 5
-    # if uri.instance_of? URI::HTTPS
-    #   http.use_ssl = true
-    # end
-    # response = http.request(req)
+    return nil if limit > 5
 
-    Typhoeus.get url
+    Typhoeus.get url, followlocation: true, connecttimeout: 5
+  rescue StandardError => e
+    Honeybadger.notify "Problem retrieving RSS content: #{e.message}"
   end
 
+  # @param [String] url
+  # @param [Fixnum] lifetime
   def rss_proxy(url, lifetime = 900)
-    struct = RSSCache.instance.get(url)
-    if !struct || (Time.now.to_i > struct[:timestamp] + lifetime)
-      begin
-        feed_response = make_request(url)
-      rescue
-        Rails.logger.warn("rss_proxy: error fetching URL: #{url}")
-        Honeybadger.notify "Problem retrieving RSS content: #{feed_response.try :body}"
-      end
+    data = RSSCache.instance.get(url)
+    if !data || (Time.now.to_i > data[:timestamp] + lifetime)
+      feed_response = make_request(url)
       if feed_response
-        struct = {
+        data = {
           content: feed_response.body,
           content_type: feed_response.headers['Content-Type'],
           timestamp: Time.now.to_i,
         }
-        RSSCache.instance.store(url, struct)
+        RSSCache.instance.store(url, data)
       end
     end
-    response.headers['Content-Type'] = struct[:content_type]
-    render text: struct[:content]
+    response.headers['Content-Type'] = data[:content_type]
+    render text: data[:content]
   end
-
 end
