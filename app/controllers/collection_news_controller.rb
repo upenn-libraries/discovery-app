4c5d62344128128a2506d09667fa2bae00f439af
Mike Kanning <mkanning@upenn.edu>
2021-10-04 06:48:50 -0400

    improve use of Typhoeus in subject_specialists code. move long subject area queries into their own module. use a constant for news RSS url.

--- app/controllers/collection_news_controller.rb
+++ app/controllers/collection_news_controller.rb
@@ -1,13 +1,14 @@
+# frozen_string_literal: true
 
-require 'net/http'
 require 'singleton'
 
 class CollectionNewsController < ApplicationController
-
   include RssProxy
 
-  def index
-    rss_proxy('https://www.library.upenn.edu/blogs/libraries-news/category/Collections/rss.xml')
-  end
+  PENN_LIB_NEWS_RSS_URL =
+    'https://www.library.upenn.edu/blogs/libraries-news/category/Collections/rss.xml'
 
+  def index
+    rss_proxy PENN_LIB_NEWS_RSS_URL
+  end
 end
