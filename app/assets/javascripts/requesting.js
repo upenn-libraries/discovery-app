712cbb4d72395c357a92297695fc753b8e23d25a
Nikitas Tampakis <nikitas.tampakis@gmail.com>
2021-10-20 11:41:01 -0400

    moves course reserve and error link to footer

--- app/assets/javascripts/requesting.js
+++ app/assets/javascripts/requesting.js
@@ -94,14 +94,14 @@ function initializeRequestingWidget($widgetArea, context) {
 
         if(context === 'show') {
             $.getJSON('/request/options?mms_id=' + mmsId, function(data) {
-                var $showTools = $('.show-tools .panel-body ul.nav');
+                var $showTools = $('ul.other-view-links');
                 var ares_url = data['ARES'];
                 var enhance_url = data['ENHANCED'];
                 if(ares_url) {
-                    $showTools.append($('<li><a target="_blank" href="'+ ares_url +'">Place on Course Reserve</a></li>'));
+                    $showTools.append($('<li> | <a target="_blank" href="'+ ares_url +'">Place on Course Reserve</a></li>'));
                 }
                 if(enhance_url) {
-                    $showTools.append($('<li><a target="_blank" href="'+ enhance_url +'">Report Cataloging Error</a></li>'));
+                    $showTools.append($('<li> | <a target="_blank" href="'+ enhance_url +'">Report Cataloging Error</a></li>'));
                 }
             });
         }
