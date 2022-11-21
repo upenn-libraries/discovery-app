3a02d2d580b17e5db40db4dc0476d83e0b38eeb8
Nikitas Tampakis <tampakis@upenn.edu>
2022-11-21 16:37:39 -0500

    updates rss feed url to new library website

--- app/controllers/collection_news_controller.rb
+++ app/controllers/collection_news_controller.rb
@@ -6,7 +6,7 @@ class CollectionNewsController < ApplicationController
   include RssProxy
 
   PENN_LIB_NEWS_RSS_URL =
-    'https://old.library.upenn.edu/blogs/libraries-news/category/Collections/rss.xml'
+    'https://www.library.upenn.edu/penn-libraries-news/collections/rss.xml'
 
   def index
     rss_proxy PENN_LIB_NEWS_RSS_URL
