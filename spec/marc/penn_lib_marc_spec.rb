f69170862e3959bf65b7c09daa2f8164e4b16b03
Mike Kanning <mkanning@upenn.edu>
2023-01-18 16:51:52 -0500

    add a spec to the MARC indexing specs that covers happy path for generating a bookplate image (link) for a record. add comments to marc file with some explanation of cryptic code.

--- spec/marc/penn_lib_marc_spec.rb
+++ spec/marc/penn_lib_marc_spec.rb
@@ -23,10 +23,10 @@ RSpec.describe PennLib::SubjectConfig, type: :model do
 end
 
 RSpec.describe PennLib::Marc, type: :model do
-  let(:marc) { PennLib::Marc.new(PennLib::CodeMappings.new(Rails.root.join('config/translation_maps')) }
+  let(:marc) { PennLib::Marc.new(PennLib::CodeMappings.new(Rails.root.join('config/translation_maps'))) }
   let(:rec) do
     MARC::XMLReader.new(
-      Rails.root.join('spec/fixtures/marcxml/9978004977403681.xml'),
+      Rails.root.join('spec/fixtures/marcxml/9978004977403681.xml').to_s,
       parser: :nokogiri
     ).first
   end
@@ -58,6 +58,27 @@ RSpec.describe PennLib::Marc, type: :model do
     it 'does not include URIs from $1' do
       names = marc.get_author_creator_2_search_values(rec)
       expect(names.first).not_to include '123456789'
+    end
+  end
+  context 'web link parsing for display' do
+    context 'for a fund-based bookplate' do
+      let(:weblink_rec) do
+        MARC::XMLReader.new(
+          Rails.root.join('spec/fixtures/marcxml/bookplated_record.xml').to_s,
+          parser: :nokogiri
+        ).first
+      end
+      let(:web_links) { marc.get_web_link_display(weblink_rec) }
+      it 'has two web link entries' do
+        expect(web_links.length).to eq 2
+      end
+      it 'has a bookplate img link entry with expected structure' do
+        expect(web_links[1]).to eq({
+          img_src: 'https://old.library.upenn.edu/sites/default/files/images/bookplates/SmidtFamilyModernContemporaryArt.gif',
+            img_alt: 'The Smidt Family Modern and Contemporary Art Collection Fund Home Page  Bookplate',
+            linkurl: 'http://hdl.library.upenn.edu/1017.12/2554579'
+        })
+      end
     end
   end
 end
